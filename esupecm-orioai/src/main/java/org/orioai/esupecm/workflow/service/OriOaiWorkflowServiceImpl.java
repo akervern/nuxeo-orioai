2b125cd8a2fe654b4987f8b6cffdc508d9d0fa62
Arnaud Kervern <akervern@nuxeo.com>
2013-09-30 11:56:20 +0200

    Remove Xfire dependency

--- esupecm-orioai/src/main/java/org/orioai/esupecm/workflow/service/OriOaiWorkflowServiceImpl.java
+++ esupecm-orioai/src/main/java/org/orioai/esupecm/workflow/service/OriOaiWorkflowServiceImpl.java
@@ -1,6 +1,7 @@
 package org.orioai.esupecm.workflow.service;
 
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -8,11 +9,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.Vector;
 
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.codehaus.xfire.client.XFireProxyFactory;
-import org.codehaus.xfire.service.Service;
-import org.codehaus.xfire.service.binding.ObjectServiceFactory;
 import org.nuxeo.runtime.model.ComponentInstance;
 import org.nuxeo.runtime.model.DefaultComponent;
 import org.orioai.esupecm.OriOaiMetadataType;
@@ -28,20 +29,20 @@ public class OriOaiWorkflowServiceImpl extends DefaultComponent implements OriOa
 
 	protected HashMap<String, IOriWorkflowService> _oriWorkflowServices;
 
-	
+
 
 	public void registerContribution(Object contribution,
 			String extensionPoint, ComponentInstance contributor) {
 		config = (WsDescriptor) contribution;
 	}
 
-	
+
 	public void unregisterContribution(Object contribution,
 			String extensionPoint, ComponentInstance contributor) {
 		if (config==contribution)
 			config = null;
 	}
-	
+
 
 	/**
 	 * Get the ori-oai-workflow service
@@ -52,43 +53,43 @@ public class OriOaiWorkflowServiceImpl extends DefaultComponent implements OriOa
 		if (_oriWorkflowServices == null) {
 			_oriWorkflowServices = new HashMap<String, IOriWorkflowService>();
 		}
-		
+
 		IOriWorkflowService oriWorkflowService = _oriWorkflowServices.get(username);
-		
+
 		if( oriWorkflowService == null ) {
-			
+
 			String wsUrl = config.getWsUrl();
 
 			log.info("getRemoteOriWorkflowService :: contacting Web Service from URL : " + wsUrl);
 
-			Service serviceModel = new ObjectServiceFactory().create( IOriWorkflowService.class);
-
 			try {
-				oriWorkflowService = (IOriWorkflowService) new XFireProxyFactory().create(serviceModel, wsUrl);
+                QName service_name = new QName("http://remote.services.workflow.orioai.org", "OriWorkflowServiceService");
+                Service service = Service.create(new URL(wsUrl + "?wsdl"), service_name);
+				oriWorkflowService = service.getPort(IOriWorkflowService.class);
 				_oriWorkflowServices.put(username, oriWorkflowService);
 			}
 			catch (MalformedURLException e) {
 				throw new RuntimeException("pb retireving ori-oai-workflow Web Service", e);
 			}
 		}
-		
+
 		return oriWorkflowService;
 	}
 
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 	public List<OriOaiMetadataType> getMetadataTypes(String username) {
 
-		
+
 		if (log.isDebugEnabled())
 			log.debug("getMetadataTypes :: going to get metadataTypes for "+username);
-		
+
 		IOriWorkflowService oriWorkflowService = getRemoteOriWorkflowService(username);
-		
+
 		Map<String,String> metadataTypes = oriWorkflowService.getMetadataTypes(username);
 		if (log.isDebugEnabled())
 			log.debug("getMetadataTypes :: get metadataTypes for "+username+" from Web Service ok : " + metadataTypes.toString());
@@ -98,18 +99,18 @@ public class OriOaiWorkflowServiceImpl extends DefaultComponent implements OriOa
 		while (metadataTypeIds.hasNext()) {
 			String metadataTypeId = metadataTypeIds.next();
 			String metadataTypeLabel = metadataTypes.get(metadataTypeId);
-			
+
 			OriOaiMetadataType metadataType = new OriOaiMetadataType(metadataTypeId, metadataTypeLabel);
-			
+
 			oriOaiMetadataTypes.add(metadataType);
 		}
-		
+
 		return oriOaiMetadataTypes;
 	}
-	
-	
-	
-	
+
+
+
+
 	public Long newWorkflowInstance(String username, String metadataTypeId) {
 
 		IOriWorkflowService oriWorkflowService = getRemoteOriWorkflowService(username);
@@ -117,11 +118,11 @@ public class OriOaiWorkflowServiceImpl extends DefaultComponent implements OriOa
 		log.debug("newWorkflowInstance :: call newWorkflowInstance from Web Service ...");
 
 		Long id = oriWorkflowService.newWorkflowInstance(null, metadataTypeId, username);
-		
+
 		return id;
 	}
 
-	
+
 
 	/**
 	 * @deprecated
@@ -129,39 +130,39 @@ public class OriOaiWorkflowServiceImpl extends DefaultComponent implements OriOa
 	 */
 	public Map<String, String> getCurrentStates(String username, String idp, String language) {
 		IOriWorkflowService oriWorkflowService = getRemoteOriWorkflowService(username);
-		
+
 		Map<String, String> currentStates = oriWorkflowService.getCurrentStates(idp, language);
 		if (log.isDebugEnabled())
 			log.debug("getCurrentStates :: currentStates=" + currentStates);
-		
+
 		return currentStates;
 	}
-	
+
 	public Vector<String> getCurrentStates(Map<String, String> statesMap) {
 		Vector<String> currentStates = new Vector<String>();
 		for (String key : statesMap.keySet())
 			currentStates.add(statesMap.get(key));
-		
+
 		return currentStates;
 	}
-	
+
 	/**
 	 * @deprecated
 	 * use List<String> getCurrentInformations(Map<String, String> currentInformations) instead
 	 */
 	public List<String> getCurrentInformations(String username, String idp, String language) {
-		
+
 		IOriWorkflowService oriWorkflowService = getRemoteOriWorkflowService(username);
-		
+
 		if (log.isDebugEnabled()) {
 			log.debug("getCurrentInformations :: idp=" + idp);
 			log.debug("getCurrentInformations :: language=" + language);
 		}
-		
+
 		Map<String, String> currentInformations = oriWorkflowService.getErrors(idp, language);
 		if (log.isDebugEnabled())
 			log.debug("getCurrentInformations :: currentInformations=" + currentInformations);
-		
+
 		List<String> informations = new ArrayList<String>();
 
 		for (Map.Entry<String, String> entry : currentInformations.entrySet()) {
@@ -172,11 +173,11 @@ public class OriOaiWorkflowServiceImpl extends DefaultComponent implements OriOa
 
 		return informations;
 	}
-	
+
 	public List<String> getCurrentInformations(Map<String, String> currentInformations) {
 		if (log.isDebugEnabled())
 			log.debug("getCurrentInformations :: currentInformations=" + currentInformations);
-		
+
 		List<String> informations = new ArrayList<String>();
 
 		for (Map.Entry<String, String> entry : currentInformations.entrySet()) {
@@ -187,8 +188,8 @@ public class OriOaiWorkflowServiceImpl extends DefaultComponent implements OriOa
 
 		return informations;
 	}
-	
-	
+
+
 
 	/**
 	 * Return metadata type for a given idp (remote access to workflow)
@@ -199,17 +200,17 @@ public class OriOaiWorkflowServiceImpl extends DefaultComponent implements OriOa
 		IOriWorkflowService oriWorkflowService = getRemoteOriWorkflowService(username);
 		try {
 			String metadataTypeId = oriWorkflowService.getMetadataType(idp);
-			
+
 			return new OriOaiMetadataType(metadataTypeId, oriWorkflowService.getMetadataTypes().get(metadataTypeId));
 		}
 		catch (Exception e) {
 			log.error("getMetadataType :: can't retrieve metadata type from idp "+ idp, e);
 			return null;
 		}
-		
+
 	}
 
-	
+
 	public String getMetadataSchemaNamespace(String username, String metadataTypeId) {
 		IOriWorkflowService oriWorkflowService = getRemoteOriWorkflowService(username);
 		try {
@@ -222,17 +223,17 @@ public class OriOaiWorkflowServiceImpl extends DefaultComponent implements OriOa
 			return null;
 		}
 	}
-	
-	
-	
-	
+
+
+
+
 	public String getIdp(String username, Long id) {
 		IOriWorkflowService oriWorkflowService = getRemoteOriWorkflowService(username);
 		return oriWorkflowService.getIdp(id, username);
 	}
 
-	
-	
+
+
 	private String getReplacedMdEditorUrl(String mdEditorUrl) {
 		if (log.isDebugEnabled()) {
 			log.debug("getReplacedMdEditorUrl :: mdEditorUrl=" + mdEditorUrl);
@@ -240,19 +241,19 @@ public class OriOaiWorkflowServiceImpl extends DefaultComponent implements OriOa
 			log.debug("getReplacedMdEditorUrl :: config.getMdEditorToUrl()=" + config.getMdEditorToUrl());
 			log.debug("getReplacedMdEditorUrl :: config.isMdEditorTranslationSet()=" + config.isMdEditorTranslationSet());
 		}
-		
+
 		String result = mdEditorUrl;
-		
+
 		if (config.isMdEditorTranslationSet()) {
 			result = mdEditorUrl.replaceFirst(config.getMdEditorFromUrl(), config.getMdEditorToUrl());
 		}
-		
+
 		return result;
 	}
-	
-	
-	
-	
+
+
+
+
 	/**
 	 * Returns md editor url for the first form available for user
 	 * @param idp
@@ -261,12 +262,12 @@ public class OriOaiWorkflowServiceImpl extends DefaultComponent implements OriOa
 	 */
 	public String getMdeditorUrl(String username, String idp) {
 		return getMdeditorUrlWS(username, idp);
-		
+
 	}
-	
-	
+
+
 	/**
-	 * 
+	 *
 	 * @param idp
 	 * @param userId
 	 * @return
@@ -274,18 +275,18 @@ public class OriOaiWorkflowServiceImpl extends DefaultComponent implements OriOa
 	public String getMdeditorUrlWS(String username, String idp) {
 		IOriWorkflowService oriWorkflowService = getRemoteOriWorkflowService(username);
 		Map<String,String> formsMap = oriWorkflowService.getMdEditorUrl(idp);
-		
+
 		if (log.isDebugEnabled())
 			log.debug("getMdeditorUrlWS :: formsMap.get(0)=" + formsMap.get(0));
-		
+
 		String result = getReplacedMdEditorUrl(formsMap.get(0));
-		
+
 		if (log.isDebugEnabled())
 			log.debug("getMdeditorUrlWS :: result=" + result);
-		
+
 		return result;
 	}
-	
+
 	/**
 	 * Return a map for availables md editors
 	 * @param idp
@@ -295,28 +296,28 @@ public class OriOaiWorkflowServiceImpl extends DefaultComponent implements OriOa
 	public Map<String,String> getMdeditorUrls(String username, String idp) {
 		IOriWorkflowService oriWorkflowService = getRemoteOriWorkflowService(username);
 		Map<String,String> formsMap = oriWorkflowService.getMdEditorUrl(idp);
-		
+
 		if (log.isDebugEnabled())
 			log.debug("getMdeditorUrlWS :: before formsMap=" + formsMap);
-		
+
 		for (String key : formsMap.keySet()) {
 			String value = getReplacedMdEditorUrl(formsMap.get(key));
 			if (log.isDebugEnabled())
 				log.debug("getMdeditorUrlWS :: put(" + key + ", "+value+")");
 			formsMap.put(key, value);
 		}
-		
+
 		if (log.isDebugEnabled())
 			log.debug("getMdeditorUrlWS :: after formsMap=" + formsMap);
-		
+
 		return formsMap;
 	}
-	
 
-	
-	
-	
-	
+
+
+
+
+
 	/**
 	 * Returns availables actions for given user and idp
 	 * @param idp
@@ -324,21 +325,21 @@ public class OriOaiWorkflowServiceImpl extends DefaultComponent implements OriOa
 	 * @return
 	 */
 	public Map<String,String> getAvailableActions(String username, String idp, String language) {
-		
+
 		if (log.isDebugEnabled()) {
 			log.debug("getAvailableActions :: idp=" + idp);
 			log.debug("getAvailableActions :: language=" + language);
 		}
-		
+
 		IOriWorkflowService oriWorkflowService = getRemoteOriWorkflowService(username);
 		Map<String,String> actionsMap = oriWorkflowService.getAvailableActions(idp, language);
-		
+
 		if (log.isDebugEnabled())
 			log.debug("getAvailableActions :: actionsMap=" + actionsMap);
-		
+
 		return actionsMap;
 	}
-	
+
 
 	/**
 	 * Perform an available action
@@ -351,28 +352,28 @@ public class OriOaiWorkflowServiceImpl extends DefaultComponent implements OriOa
 		log.info("performAction :: idp=" + idp+", actionId="+actionId+", observation="+observation);
 		IOriWorkflowService oriWorkflowService = getRemoteOriWorkflowService(username);
 		return oriWorkflowService.performAction(idp, actionId, observation);
-		
+
 	}
-	
-	
+
+
 	public String getXMLForms(String username, String idp) {
 		IOriWorkflowService oriWorkflowService = getRemoteOriWorkflowService(username);
-		
+
 		String xml =  oriWorkflowService.getXMLForms(idp);
 		if (log.isDebugEnabled())
 			log.debug("getXMLForms :: xml=" + xml);
-		
+
 		return xml;
 	}
-	
-	
-	
+
+
+
 	public void saveXML(String username, String idp, String xmlContent) {
 		IOriWorkflowService oriWorkflowService = getRemoteOriWorkflowService(username);
-		
+
 		oriWorkflowService.saveXML(idp, xmlContent);
 	}
-	
+
 	public InstanceInfos getInstanceInfos(Long id, String userId, String language) {
 		IOriWorkflowService oriWorkflowService = getRemoteOriWorkflowService(userId);
 		return oriWorkflowService.getInstanceInfos(id, userId, language);
